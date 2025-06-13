8b7ddd11705e8f2d7643de468b130968ba65cf9c
Kartika Salsabila <rindoupaddlepop@gmail.com>
2025-06-13 21:23:48 +0700

    Add files via upload

---
+++ Praktikum 9/praktikum9.sql
@@ -0,0 +1,84 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 12, 2025 at 05:34 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `praktikum9`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `view_mhs_simple` ()   BEGIN
+	SELECT NPM, nama, no_hp
+    FROM mahasiswa;
+END$$
+
+--
+-- Functions
+--
+CREATE DEFINER=`root`@`localhost` FUNCTION `tambah` (`angka1` INT, `angka2` INT) RETURNS INT(11)  BEGIN
+	RETURN angka1 + angka2;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `NPM` int(11) NOT NULL,
+  `nama` varchar(100) DEFAULT NULL,
+  `alamat` varchar(100) DEFAULT NULL,
+  `id_kelurahan` int(11) DEFAULT NULL,
+  `jenis_kelamin` char(1) DEFAULT NULL,
+  `no_hp` varchar(15) DEFAULT NULL,
+  `kode_prodi` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`NPM`, `nama`, `alamat`, `id_kelurahan`, `jenis_kelamin`, `no_hp`, `kode_prodi`) VALUES
+(12345, 'Imam Adi Nata', 'Kajen', 101, 'L', '081215529899', 1),
+(12346, 'Budi Sugandhi', 'Bekasi', 109, 'L', '085643433321', 2),
+(12347, 'Toha Sitohang', 'Medan', 103, 'L', '08989787876', 1),
+(12348, 'Megawati', 'Condong Catur', 107, 'P', '0839303058', 1),
+(12349, 'PRABOWO SUBIYANTO', 'JKT', 107, 'L', '08132089811', NULL),
+(12780, 'Anis Baswedan', 'Jakarta', 234, 'L', '09898877663', NULL),
+(12786, 'GANJAR PRANOWO', 'JAWA TENGAH', 123, 'L', '087656356622', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`NPM`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
