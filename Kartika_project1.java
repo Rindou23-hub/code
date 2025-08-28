af6a65b28e014432c7541c20caac4a1302accf5a
Kartika Salsabila <rindoupaddlepop@gmail.com>
2025-08-28 23:34:05 +0700

    Add files via upload

---
+++ Kartika_project1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.kartika_project1;
+
+/**
+ *
+ * @author user
+ */
+public class Kartika_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama : Kartika_Salsabila");
+        System.out.println("NPM : 2410506003");
+        System.out.println("Alamat : Rindam_4_DIP");
+        System.out.println("No_HP: 081325963078");
+    }
+}
