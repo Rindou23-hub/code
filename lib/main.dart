d83889a12f4805b9339e58b06ffc8a59d07bcaec
Kartika Salsabila <rindoupaddlepop@gmail.com>
2026-03-18 10:14:59 +0700

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: const Color.fromARGB(246, 11, 160, 230),
-        appBar: AppBar(
-          title: Text("Cihuyyy!"),
-          backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-        ),
-        body: Center(
-          child: Image(image: AssetImage('images/Glass_Flower.jpg')),
+        backgroundColor: const Color.fromARGB(255, 79, 118, 200),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage("images/profile.jpg"),
+              ),
+              Text(
+                "Kartika Salsabila",
+                style: GoogleFonts.pacifico(
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "ANDROID DEVELOPER",
+                style: GoogleFonts.sourceSans3(
+                  fontSize: 20,
+                  color: Colors.teal.shade100,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(color: Colors.teal.shade100),
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: Row(
+                  children: [
+                    Icon(Icons.phone, color: Colors.black),
+                    SizedBox(width: 10),
+                    Text(
+                      "081325963078",
+                      style: GoogleFonts.sourceSans3(fontSize: 20),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: Row(
+                  children: [
+                    Icon(Icons.email, color: Colors.black),
+                    SizedBox(width: 10),
+                    Text(
+                      "salsabilakartika01@gmail.com",
+                      style: GoogleFonts.sourceSans3(fontSize: 20),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
